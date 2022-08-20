b9671ade104b08a505047854fcc79e1465f8be7b
aakashdeveloper <ahanda205@gmail.com>
2022-08-20 09:40:56 +0530

    reactiveforms

--- reactiveform/src/app/userform/userForm.component.ts
+++ reactiveform/src/app/userform/userForm.component.ts
@@ -1,6 +1,15 @@
 import {Component,OnInit} from '@angular/core';
 import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
+function ratingRange(min:Number,max:Number){
+    return(out: AbstractControl):{[key:string]:boolean} | null => {
+        if(out.value !== undefined && isNaN(out.value) || out.value < min || out.value > max){
+            return {range:true}
+        }else{
+            return null
+        }
+    }
+}
 
 @Component({
     selector:'app-forms',
@@ -13,8 +22,28 @@ export class UserComponent implements OnInit{
 
     ngOnInit(){
         this.employeeForm = this.fb.group({
-            firstName :['John',[Validators.required,Validators.minLength(3)]]
+            firstName :['John',[Validators.required,Validators.minLength(3)]],
+            lastName: ['Dave',[Validators.required,Validators.maxLength(5)]],
+            email: ['a@a.com',[Validators.required,
+                               Validators.pattern('^([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$')]],
+            phone:[''],
+            Notification:[],
+            rating:['3',[Validators.required, ratingRange(1,5)]]
         })
+    }
+
+    setNotification(notifyVia: string):void{
+        const phoneControl = this.employeeForm.get('phone');
+        if(notifyVia === 'phone'){
+            phoneControl.setValidators(Validators.required)
+        }else{
+            phoneControl.clearValidators()
+        }
+        phoneControl.updateValueAndValidity()
+    }
+
+    submitForm():void{
+        console.log(this.employeeForm.value)
     }
 }
 
